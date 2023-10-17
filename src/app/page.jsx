467f994ea789d534f4c77578a57647f2bbd9302a
macirander <maciranderfilho@hotmail.com>
2023-10-17 10:21:18 -0300

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <h1>HOME PAGE</h1>
+      <h1>HOME PAGE</h1>
+
+      <div>
+        <Link href="/produtos/tenis">TÊNIS</Link> | 
+        <Link href="/produtos/produto/tenis-preto">TÊNIS PRETO</Link> |
+        <Link href="/produtos/produto/camisa-xadrez">CAMISA-X</Link>
+      </div>
+
     </>
   )
 }
